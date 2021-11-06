725177a263e1a20007447f99008372fdc7d03f05
kiwvic <you@example.com>
2021-11-06 22:31:01 +0200

    added motor

---
+++ adb.py
@@ -0,0 +1,42 @@
+import pymongo
+import asyncio
+import motor.motor_asyncio
+from loader import CONNECT_STR_MONGO, DATABASE_NAME_MONGO
+
+
+client = motor.motor_asyncio.AsyncIOMotorClient(CONNECT_STR_MONGO)
+db = client[DATABASE_NAME_MONGO]
+
+C_USERS = db["users"]
+
+
+async def insert_document(collection, data: dict, multiple=False) -> None or int:
+    if multiple:
+        await collection.insert_many(data)
+    else:
+        await collection.insert_one(data)
+
+
+async def find_document(collection, elements: dict, multiple=False) -> list or dict:
+    if multiple:
+        answer = []
+        async for document in collection.find(elements):
+            answer.append(document)
+        return answer
+    else:
+        return await collection.find_one(elements)
+
+
+async def get_all_documents(collection) -> list:
+    answer = list()
+    async for doc in collection.find():
+        answer.append(doc)
+    return answer
+
+
+async def delete_document(collection, id_) -> None:
+    await collection.delete_one({"_id": id_})
+
+
+async def update_document(collection, id_, doc: dict) -> None:
+    await collection.update_one({"_id": id_}, {'$set': doc})
--- loader.py
+++ loader.py
@@ -4,6 +4,8 @@ import os, dotenv
 
 dotenv.load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+CONNECT_STR_MONGO = os.getenv("CONNECT_STR_MONGO")
+DATABASE_NAME_MONGO = os.getenv("DATABASE_NAME_MONGO")
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
